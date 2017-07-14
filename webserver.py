d0dd5eebff0aa81e490fc26083f18620e41ede9b
thaher <thaherster@gmail.com>
2017-07-14 08:27:22 +0530

    server error

--- webserver.py
+++ webserver.py
@@ -10,7 +10,9 @@ class webserverHandler(BaseHTTPRequestHandler):
 				self.end_headers()
 
 				output=""
-				output+= "<html><body>Hello!</body></html>"
+				output+= "<html><body>Hola!<a href='/hello'>Back to Hello</a>"
+				output+= "<form method='POST' enctype='multipart/form-data' action='/hello'><h2>What would you like me to say ?</h2><input name='message' type='text' ><input type='submit' value='Submit'></form>"	
+				output+= "</body></html>"
 				self.wfile.write(output.encode())
 				print (output)
 				return
@@ -20,7 +22,9 @@ class webserverHandler(BaseHTTPRequestHandler):
 				self.end_headers()
 
 				output=""
-				output+= "<html><body>Hola!<a href='/hello'>Back to Hello</a></body></html>"
+				output+= "<html><body>Hola!<a href='/hello'>Back to Hello</a>"
+				output+= "<form method='POST' enctype='multipart/form-data' action='/hello'><h2>What would you like me to say ?</h2><input name='message' type='text' ><input type='submit' value='Submit'></form>"	
+				output+= "</body></html>"
 				self.wfile.write(output.encode())
 				print (output)
 				return
@@ -28,14 +32,29 @@ class webserverHandler(BaseHTTPRequestHandler):
 		except IOError:
 			self.send_error(404,"File Not Found %s" % self.path)
 
-	def do_POST:
+	def do_POST(self):
 		try:
 			self.send_response(301)
-			self.send_headers()
+			self.send_header('Content-type','text/html')
+			self.end_headers()
 
-			ctype
+			ctype, pdict = cgi.parse_header(self.headers.get_all('content-type'))
+			if ctype == 'multipart/form-data':
+				fields=cgi.parse_multipart(self.rfile,pdict)
+				messagecontent = fields.get('message')
 
-		except:
+				output=""
+				output+= "<html><body>"
+				output+= "<h2>okay how about this: </h2>"	
+				output+= "<h1> %s </h1>" % messagecontent[0]						
+				output+= "<form method='POST' enctype='multipart/form-data' action='/hello'><h2>What would you like me to say ?</h2><input name='message' type='text' ><input type='submit' value='Submit'></form>"	
+				output+= "</body></html>"
+				self.wfile.write(output.encode())
+				print(output)
+				return
+
+		except IOError:
+			self.send_error(404,"File Not Found %s" % self.path)
 
 
 def main():
